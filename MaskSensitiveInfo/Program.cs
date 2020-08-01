59a2c7bf64e7ea0db15d22e907b5ec03eaa04219
Aaron Lain <aaron.j.lain@gmail.com>
2020-08-01 12:30:07 -0500

    works

--- MaskSensitiveInfo/Program.cs
+++ MaskSensitiveInfo/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MaskSensitiveInfo
 {
     class Program
     {
+        static void Mask(string input)
+        {
+            List<string> temp = new List<string>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == 0)
+                {
+                    temp.Add(input[i].ToString());
+                }
+                else if (i != 0)
+                {
+                    temp.Add("*");
+                    
+                }
+            }
+            string output = String.Join("", temp);
+            Console.WriteLine($"{output}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            do
+            {
+                Console.WriteLine("Give me a secret to hide:");
+                input = Console.ReadLine();
+                Mask(input);
+
+            } while (input != "exit" && input != "");
         }
     }
 }
