b5d50f95ecd04fb0169293e27fbe8ae709d46bf8
Aaron Lain <aaron.j.lain@gmail.com>
2020-08-01 12:09:38 -0500

    Add project files.

---
+++ MaskSensitiveInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
